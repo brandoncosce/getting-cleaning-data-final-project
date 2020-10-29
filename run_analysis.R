90a8391514d1bf857c42c6af37882056b67b43d6
brandoncosce <55424709+brandoncosce@users.noreply.github.com>
2020-10-28 17:29:20 -0700

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,18 @@
+onedatasetx <- bind_rows(X_test,X_train)
+onedatasety <- bind_rows(y_test,y_train)
+colnames(onedatasetx) <- features
+colnames(onedatasety) <- "train"
+onedataset <- bind_cols(onedatasety,onedatasetx)
+m.std.colnames <- grep("mean|std|train",colnames(onedataset))
+m.std <- onedataset[,m.std.colnames]
+colnames(activity_labels) <- c("train","activity")
+m.std.act_names <- merge(m.std,activity_labels, by.x = "train",by.y = "train")
+tidy_data <- m.std.act_names %>% select(activity,train,everything())
+tidy_data_grp <- tidy_data %>% group_by(activity)
+new_tidy_data <- as.data.frame(unique(activity_labels))
+
+m <-  tidy_data_grp %>% summarise(across(everything(), mean))
+m <- m[-c(2)]
+
+
+write.table(m,"final-tidy-data.txt",row.names = FALSE)
